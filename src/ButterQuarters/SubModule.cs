6d155d31c0af1ab651b14c1f83b4b0e07ce84aea
J. Zebedee <zebedee@code.gripe>
2022-11-03 02:47:08 -0500

    Update

--- src/ButterQuarters/SubModule.cs
+++ src/ButterQuarters/SubModule.cs
@@ -1,13 +1,21 @@
-﻿using TaleWorlds.MountAndBlade;
+﻿using HarmonyLib;
+using System.Reflection;
+using System;
+using TaleWorlds.MountAndBlade;
 
 namespace ButterQuarters
 {
     public class SubModule : MBSubModuleBase
     {
+        private const string HarmonyId = $"{nameof(ButterQuarters)}.harmony";
+
+        private readonly Lazy<Harmony> _harmony = new(() => new Harmony(HarmonyId));
+
         protected override void OnSubModuleLoad()
         {
             base.OnSubModuleLoad();
 
+            _harmony.Value.PatchAll(Assembly.GetExecutingAssembly());
         }
 
         protected override void OnSubModuleUnloaded()
