585a8961eac8735beff8a526537a57c655e52417
J. Zebedee <zebedee@code.gripe>
2022-11-22 00:22:23 -0600

    Add EquipmentExtensions

---
+++ src/ButterEquipped/AutoEquip/EquipmentExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using TaleWorlds.Core;
+
+namespace ButterEquipped.AutoEquip;
+
+internal static class EquipmentExtensions
+{
+    public static bool HasWeaponOfClass(this Equipment equipment, params WeaponClass[] weaponClasses)
+    {
+        for (EquipmentIndex index = EquipmentIndex.WeaponItemBeginSlot; index < EquipmentIndex.NumAllWeaponSlots; index++)
+        {
+            var eqEl = equipment[index];
+            if (eqEl.IsEmpty)
+            {
+                continue;
+            }
+
+            if (weaponClasses.Contains(eqEl.Item.PrimaryWeapon.WeaponClass))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
