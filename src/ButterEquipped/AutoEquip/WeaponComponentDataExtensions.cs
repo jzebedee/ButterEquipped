f43886d9dc924a3cdc76e93ad67bdc72ee1c84df
J. Zebedee <zebedee@code.gripe>
2022-11-22 18:30:24 -0600

    Add GetUsageFlags extension

---
+++ src/ButterEquipped/AutoEquip/WeaponComponentDataExtensions.cs
@@ -0,0 +1,15 @@
+﻿using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+using static TaleWorlds.Core.ItemObject;
+
+namespace ButterEquipped.AutoEquip;
+
+internal static class WeaponComponentDataExtensions
+{
+    public static ItemUsageSetFlags GetUsageFlags(this WeaponComponentData wcd)
+    => wcd.ItemUsage switch
+    {
+        string usage => MBItem.GetItemUsageSetFlags(usage),
+        _ => default
+    };
+}
