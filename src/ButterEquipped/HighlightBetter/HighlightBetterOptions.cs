d4217da24f9b94a010f667e7f19025e91fb0be75
J. Zebedee <zebedee@code.gripe>
2024-01-07 17:41:25 -0600

    Add HighlightBetterOptions

---
+++ src/ButterEquipped/HighlightBetter/HighlightBetterOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace ButterEquipped.HighlightBetter;
+
+public record class HighlightBetterOptions
+{
+    public bool HighlightBetterItems { get; set; } = true;
+    public bool HighlightFromLoot { get; set; } = true;
+    public bool HighlightFromStash { get; set; } = true;
+    public bool HighlightFromInventory { get; set; } = true;
+    public bool HighlightFromDiscard { get; set; } = true;
+    public bool HighlightFromTrade { get; set; } = false;
+}
