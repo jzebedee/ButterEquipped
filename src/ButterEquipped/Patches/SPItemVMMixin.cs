d19b6e3478deb878a9c055b551a6a15a4ac190fb
J. Zebedee <zebedee@code.gripe>
2024-01-07 20:45:32 -0600

    Add stubs for BetterHighlightOptions

--- src/ButterEquipped/Patches/SPItemVMMixin.cs
+++ src/ButterEquipped/Patches/SPItemVMMixin.cs
@@ -27,17 +27,84 @@ internal class SPItemVMMixin : BaseViewModelMixin<SPItemVM>
         get => currentInventory;
         set
         {
+            System.Diagnostics.Debug.Assert(CurrentOptions is not null);
+
             //if (currentInventory != value)
             {
                 currentInventory = value;
-                OnEquipmentUpdate?.Invoke(currentInventory);
+                OnEquipmentUpdate?.Invoke(value);
+            }
+        }
+    }
+
+    private static Action<HighlightBetterOptions>? OnOptionsUpdate;
+    private static HighlightBetterOptions? currentOptions;
+    internal static HighlightBetterOptions CurrentOptions
+    {
+        get => currentOptions!;
+        set
+        {
+            if (currentOptions != value)
+            {
+                currentOptions = value;
+                OnOptionsUpdate?.Invoke(value);
             }
         }
     }
 
     public SPItemVMMixin(SPItemVM vm) : base(vm)
     {
-        OnEquipmentUpdate += HandleEquipmentUpdate;
+        OnOptionsUpdate += HandleOptionsUpdate;
+    }
+
+    private void HandleOptionsUpdate(HighlightBetterOptions options)
+    {
+        if (options is { HighlightBetterItems: false })
+        {
+            OnEquipmentUpdate -= HandleEquipmentUpdate;
+            ButterEquippedIsItemBetter = false;
+            return;
+        }
+
+        if (ViewModel is not { InventorySide: var side })
+        {
+            return;
+        }
+
+        switch (side)
+        {
+            case InventoryLogic.InventorySide.None:
+                break;
+            case InventoryLogic.InventorySide.OtherInventory:
+                if(InventoryManager.Instance is not { CurrentMode: var mode })
+                {
+                    return;
+                }
+
+                if(ShouldHighlightOtherSide(mode))
+                {
+                    goto case InventoryLogic.InventorySide.PlayerInventory;
+                }
+                break;
+            case InventoryLogic.InventorySide.PlayerInventory:
+                OnEquipmentUpdate += HandleEquipmentUpdate;
+                _isItemBetter = null;
+                break;
+            case InventoryLogic.InventorySide.Equipment:
+                _isItemBetter = false;
+                break;
+        }
+
+        return;
+
+        bool ShouldHighlightOtherSide(InventoryMode mode) => mode switch
+        {
+            InventoryMode.Default when options.HighlightFromDiscard => true,
+            InventoryMode.Loot when options.HighlightFromLoot => true,
+            InventoryMode.Stash when options.HighlightFromStash => true,
+            InventoryMode.Trade when options.HighlightFromTrade => true,
+            _ => false
+        };
     }
 
     private void HandleEquipmentUpdate(SPInventoryVM? vm)
@@ -127,17 +194,11 @@ internal class SPItemVMMixin : BaseViewModelMixin<SPItemVM>
 
     [DataSourceProperty]
     public float ButterEquippedScore
-    {
-        get
+        => ViewModel switch
         {
-            if (ViewModel is not { ItemRosterElement.EquipmentElement: var eqEl })
-            {
-                return -1f;
-            }
-
-            return EquipmentElementComparer.CalculateEffectiveness(eqEl);
-        }
-    }
+            { ItemRosterElement.EquipmentElement: var eqEl } => EquipmentElementComparer.CalculateEffectiveness(eqEl),
+            _ => -1f
+        };
 
     [DataSourceProperty]
     public bool ButterEquippedIsItemBetter
