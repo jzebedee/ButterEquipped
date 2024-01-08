69078b84c0299f283a4600533570b296b21002d2
J. Zebedee <zebedee@code.gripe>
2024-01-08 16:43:48 -0600

    Goofy changes that are not triggering refreshes

--- src/ButterEquipped/Patches/SPItemVMMixin.cs
+++ src/ButterEquipped/Patches/SPItemVMMixin.cs
@@ -10,6 +10,7 @@ using TaleWorlds.Core;
 using TaleWorlds.Library;
 
 namespace ButterEquipped.Patches;
+using InventorySide = InventoryLogic.InventorySide;
 
 [ViewModelMixin]
 internal class SPItemVMMixin : BaseViewModelMixin<SPItemVM>
@@ -17,107 +18,89 @@ internal class SPItemVMMixin : BaseViewModelMixin<SPItemVM>
     private static readonly Func<SPInventoryVM, EquipmentIndex, SPItemVM>? GetItemFromIndex
         = AccessTools2.GetDelegate<Func<SPInventoryVM, EquipmentIndex, SPItemVM>>(typeof(SPInventoryVM), nameof(GetItemFromIndex));
 
-    private static bool IsValid
-        => GetItemFromIndex is not null && CurrentInventory is not null;
-
-    private static Action<SPInventoryVM?>? OnEquipmentUpdate;
-    private static SPInventoryVM? currentInventory;
-    internal static SPInventoryVM? CurrentInventory
-    {
-        get => currentInventory;
-        set
-        {
-            //if (currentInventory != value)
-            {
-                currentInventory = value;
-                OnEquipmentUpdate?.Invoke(value);
-            }
-        }
-    }
-
-    private static Action<HighlightBetterOptions>? OnOptionsUpdate;
-    private static HighlightBetterOptions? currentOptions;
-    internal static HighlightBetterOptions CurrentOptions
-    {
-        get => currentOptions!;
-        set
-        {
-            if (currentOptions != value)
-            {
-                currentOptions = value;
-                OnOptionsUpdate?.Invoke(value);
-            }
-        }
-    }
-
-
     public SPItemVMMixin(SPItemVM vm) : base(vm)
     {
-        OnEquipmentUpdate += HandleEquipmentUpdate;
-        OnOptionsUpdate += HandleOptionsUpdate;
     }
+
+    bool ShouldHighlightSide()
+    {
+        if (HighlightBetterBehavior.CurrentOptions is not HighlightBetterOptions options)
+        {
+            return false;
+        }
+
+        if (options is { HighlightBetterItems: false })
+        {
+            return false;
+        }
+
+        if (ViewModel is not { InventorySide: var side })
+        {
+            return false;
+        }
+
+        return side switch
+        {
+            InventorySide.PlayerInventory => options is { HighlightFromInventory: true },
+            InventorySide.OtherInventory when InventoryManager.Instance is { CurrentMode: var mode } => ShouldHighlightOtherSide(options, mode),
+            _ => true//false
+        };
+
+        static bool ShouldHighlightOtherSide(HighlightBetterOptions options, InventoryMode mode)
+            => (options, mode) switch
+            {
+                ({ HighlightFromDiscard: true }, InventoryMode.Default) => true,
+                ({ HighlightFromLoot: true }, InventoryMode.Loot) => true,
+                ({ HighlightFromStash: true }, InventoryMode.Stash) => true,
+                ({ HighlightFromTrade: true }, InventoryMode.Trade) => true,
+                _ => false
+            };
+    }
+
+    bool IsValid()
+        => ViewModel switch
+        {
+            null => false,
+            { IsFiltered: true } => false,
+            { IsEquipableItem: false } => false,
+            //Don't skip updating the property, otherwise we will have a dirty brush in this scenario:
+            //
+            // 1. Party has Hero A w/ Bad Horse equipped, inventory contains Good Horse with Riding > 10
+            //    IsItemBetter SHOULD be true because Good Horse > Bad Horse
+            // 2. Hero A has riding 1, so Good Horse has red brush because CanCharacterUseItem: false
+            // 3. Good Horse skips update because CanCharacterUseItem: false, so IsItemBetter stays false
+            // 4. Party switches to Hero B w/ Good Horse equipped
+            // 5. Good Horse gets updated, but IsItemBetter doesn't raise a change because it's already false
+            // 6. Good Horse has a green brush even though Hero B already has Good Horse equipped
+            { CanCharacterUseItem: false } => false,
+            { InventorySide: not InventorySide.PlayerInventory and not InventorySide.OtherInventory } => false,
+            _ => true
+        };
 
     private void HandleOptionsUpdate(HighlightBetterOptions options)
     {
-        //if (options is { HighlightBetterItems: false })
-        //{
-        //    OnEquipmentUpdate -= HandleEquipmentUpdate;
-        //    ButterEquippedIsItemBetter = false;
-        //    return;
-        //}
-
-        //if (ViewModel is not { InventorySide: var side })
-        //{
-        //    return;
-        //}
-
-        //switch (side)
-        //{
-        //    case InventoryLogic.InventorySide.None:
-        //        break;
-        //    case InventoryLogic.InventorySide.OtherInventory:
-        //        if (InventoryManager.Instance is not { CurrentMode: var mode })
-        //        {
-        //            return;
-        //        }
-
-        //        if (ShouldHighlightOtherSide(mode))
-        //        {
-        //            goto case InventoryLogic.InventorySide.PlayerInventory;
-        //        }
-        //        break;
-        //    case InventoryLogic.InventorySide.PlayerInventory:
-        //        OnEquipmentUpdate += HandleEquipmentUpdate;
-        //        _isItemBetter = null;
-        //        break;
-        //    case InventoryLogic.InventorySide.Equipment:
-        //        _isItemBetter = false;
-        //        break;
-        //}
-
-        //return;
-
-        //bool ShouldHighlightOtherSide(InventoryMode mode) => mode switch
-        //{
-        //    InventoryMode.Default when options.HighlightFromDiscard => true,
-        //    InventoryMode.Loot when options.HighlightFromLoot => true,
-        //    InventoryMode.Stash when options.HighlightFromStash => true,
-        //    InventoryMode.Trade when options.HighlightFromTrade => true,
-        //    _ => false
-        //};
+        if (ShouldHighlightSide())
+        {
+            ButterEquippedIsItemBetter = CompareEquipment();
+        }
+        else
+        {
+            ButterEquippedIsItemBetter = false;
+        }
     }
 
 
-    private void HandleEquipmentUpdate(SPInventoryVM? vm)
+    private void HandleEquipmentUpdate(SPInventoryVM? oldVm, SPInventoryVM? newVm)
     {
-        if (vm is null)
+        if (oldVm is not null)
         {
-            return;
+            oldVm.PropertyChangedWithValue -= HandleVmPropertyChanged;
         }
 
-        //necessary?
-        vm.PropertyChangedWithValue -= HandleVmPropertyChanged;
-        vm.PropertyChangedWithValue += HandleVmPropertyChanged;
+        if(newVm is not null)
+        {
+            newVm.PropertyChangedWithValue += HandleVmPropertyChanged;
+        }
     }
 
     private void HandleVmPropertyChanged(object? sender, PropertyChangedWithValueEventArgs e)
@@ -128,23 +111,7 @@ internal class SPItemVMMixin : BaseViewModelMixin<SPItemVM>
             return;
         }
 
-        if (ViewModel switch
-        {
-            { IsFiltered: true } => true,
-            { IsEquipableItem: false } => true,
-            //Don't skip updating the property, otherwise we will have a dirty brush in this scenario:
-            //
-            // 1. Party has Hero A w/ Bad Horse equipped, inventory contains Good Horse with Riding > 10
-            //    IsItemBetter SHOULD be true because Good Horse > Bad Horse
-            // 2. Hero A has riding 1, so Good Horse has red brush because CanCharacterUseItem: false
-            // 3. Good Horse skips update because CanCharacterUseItem: false, so IsItemBetter stays false
-            // 4. Party switches to Hero B w/ Good Horse equipped
-            // 5. Good Horse gets updated, but IsItemBetter doesn't raise a change because it's already false
-            // 6. Good Horse has a green brush even though Hero B already has Good Horse equipped
-            //{ CanCharacterUseItem: false } => true,
-            { InventorySide: InventoryLogic.InventorySide.Equipment } => true,
-            _ => false
-        })
+        if (!IsValid())
         {
             return;
         }
@@ -153,17 +120,18 @@ internal class SPItemVMMixin : BaseViewModelMixin<SPItemVM>
     }
 
     private SPItemVM? EquipmentReference
-        => (IsValid, ViewModel, CurrentInventory) switch
+        => (GetItemFromIndex, ViewModel, HighlightBetterBehavior.CurrentVm) switch
         {
-            (true, SPItemVM vm, SPInventoryVM inventoryVm) => GetItemFromIndex!(CurrentInventory, ViewModel.ItemType),
+            (var getEq and not null, SPItemVM vm, SPInventoryVM inventoryVm) => getEq(inventoryVm, vm.ItemType),
             _ => null
         };
 
     private bool CompareEquipment()
     {
-        if (ViewModel is { InventorySide: InventoryLogic.InventorySide.Equipment }
-            or { IsEquipableItem: false }
-            or { CanCharacterUseItem: false })
+        //if (ViewModel is { InventorySide: InventorySide.Equipment }
+        //    or { IsEquipableItem: false }
+        //    or { CanCharacterUseItem: false })
+        if (!IsValid())
         {
             return false;
         }
@@ -178,6 +146,11 @@ internal class SPItemVMMixin : BaseViewModelMixin<SPItemVM>
 
     private bool CompareEquipment(SPItemVM? equippedItem)
     {
+        if (!ShouldHighlightSide())
+        {
+            return false;
+        }
+
         var isWeapon = ViewModel is { ItemType: >= EquipmentIndex.WeaponItemBeginSlot and < EquipmentIndex.NonWeaponItemBeginSlot };
         //ignore weapons, their index is always Weapon0/WeaponExtra (banners) so in the future we need 
         //to fully model the auto equip logic to match them with an appropriate equipped slot
