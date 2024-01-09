48f6257035587a3cd82590efc27d6fa2bd1fa802
J. Zebedee <zebedee@code.gripe>
2024-01-09 01:57:04 -0600

    Remove dead commented code

--- src/ButterEquipped/Patches/InventoryItemTupleWidget_UpdateCivilianStatePatch.cs
+++ src/ButterEquipped/Patches/InventoryItemTupleWidget_UpdateCivilianStatePatch.cs
@@ -19,19 +19,3 @@ internal static class InventoryItemTupleWidget_UpdateCivilianStatePatch
         OnUpdateCivilianState?.Invoke(__instance);
     }
 }
-
-//[HarmonyPatch(typeof(SPInventoryVM), "UpdateFilteredStatusOfItem")]
-//internal static class InventoryItemTupleWidget_UpdateCivilianStatePatch
-//{
-//    internal static event Action<InventoryItemTupleWidget>? OnUpdateCivilianState;
-
-//    public static void Postfix(SPInventoryVM __instance, SPItemVM item)
-//    {
-//        if (__instance is { ScreenWidget: null })
-//        {
-//            return;
-//        }
-
-//        OnUpdateCivilianState?.Invoke(__instance);
-//    }
-//}
