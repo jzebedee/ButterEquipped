b18eae7a68aceeb663862c7ad0ba1521e18ff91a
J. Zebedee <zebedee@code.gripe>
2024-01-08 22:26:46 -0600

    Fix TwoWayViewModelMixin failing since WeakReference doesn't have a parameterless constructor

--- src/ButterEquipped/Patches/TwoWayViewModelMixin.cs
+++ src/ButterEquipped/Patches/TwoWayViewModelMixin.cs
@@ -10,14 +10,14 @@ internal class TwoWayViewModelMixin<TViewModel> : BaseViewModelMixin<TViewModel>
     private static readonly ConditionalWeakTable<TViewModel, WeakReference<TwoWayViewModelMixin<TViewModel>>> _twoWayMap = new();
 
     internal static WeakReference<TwoWayViewModelMixin<TViewModel>> GetVmMixin(TViewModel viewModel)
-        => _twoWayMap.GetOrCreateValue(viewModel);
+        => _twoWayMap.GetValue(viewModel, static _ => new(null!));
 
     public TwoWayViewModelMixin(TViewModel vm) : base(vm)
     {
         //not thread safe.
         //may need to synchronize ourselves since we don't have access to the net6.0+ AddOrUpdate
 
-        var weakRef = _twoWayMap.GetOrCreateValue(vm);
+        var weakRef = _twoWayMap.GetValue(vm, /* avoid closure */ static _ => new(null!));
         weakRef.SetTarget(this);
     }
 }
