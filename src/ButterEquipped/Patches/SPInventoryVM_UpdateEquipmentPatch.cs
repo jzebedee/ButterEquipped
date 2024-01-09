03d8dc62fa95ee54793e566032dc6967d96f1f83
J. Zebedee <zebedee@code.gripe>
2024-01-09 01:57:21 -0600

    Remove unused parameter from UpdateEquipmentPatch

--- src/ButterEquipped/Patches/SPInventoryVM_UpdateEquipmentPatch.cs
+++ src/ButterEquipped/Patches/SPInventoryVM_UpdateEquipmentPatch.cs
@@ -8,10 +8,10 @@ namespace ButterEquipped.Patches;
 [HarmonyPatch(typeof(SPInventoryVM), "UpdateEquipment")]
 internal static class SPInventoryVM_UpdateEquipmentPatch
 {
-    internal static event Action<SPInventoryVM, SPItemVM, EquipmentIndex>? OnUpdateEquipment;
+    internal static event Action<SPInventoryVM, /*SPItemVM,*/ EquipmentIndex>? OnUpdateEquipment;
 
-    public static void Postfix(SPInventoryVM __instance, SPItemVM itemVM, EquipmentIndex itemType)
+    public static void Postfix(SPInventoryVM __instance, /*SPItemVM itemVM,*/ EquipmentIndex itemType)
     {
-        OnUpdateEquipment?.Invoke(__instance, itemVM, itemType);
+        OnUpdateEquipment?.Invoke(__instance, /*itemVM,*/ itemType);
     }
 }
