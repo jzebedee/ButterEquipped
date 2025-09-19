80fd48860617825670f348c449361483735458de
J. Zebedee <zebedee@code.gripe>
2025-09-18 20:43:10 -0500

    Rename InventoryManager to InventoryScreenHelper

--- src/ButterEquipped/Patches/InventoryScreenHelper_CloseInventoryPresentationPatch.cs
+++ src/ButterEquipped/Patches/InventoryScreenHelper_CloseInventoryPresentationPatch.cs
@@ -1,4 +1,18 @@
-﻿using HarmonyLib;
+﻿//using HarmonyLib;
+//using Helpers;
+//using System;
+
+//namespace ButterEquipped.Patches;
+
+//[HarmonyPatch(typeof(InventoryScreenHelper), "CloseInventoryPresentation")]
+//internal static class InventoryScreenHelper_CloseInventoryPresentationPatch
+//{
+//    internal static event Action<bool>? OnClosing;
+
+//    public static void Prefix(/*object __instance, */bool fromCancel)
+//        => OnClosing?.Invoke(fromCancel);
+//}
+using HarmonyLib;
 using System;
 using TaleWorlds.CampaignSystem.Inventory;
 
