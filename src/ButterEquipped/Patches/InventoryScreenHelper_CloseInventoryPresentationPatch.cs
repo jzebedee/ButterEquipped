52a9daf1d79a01880ee0868497fc502c9d070a06
J. Zebedee <zebedee@code.gripe>
2025-09-18 20:43:24 -0500

    Update InventoryScreenHelper patch

--- src/ButterEquipped/Patches/InventoryScreenHelper_CloseInventoryPresentationPatch.cs
+++ src/ButterEquipped/Patches/InventoryScreenHelper_CloseInventoryPresentationPatch.cs
@@ -1,30 +1,14 @@
-﻿//using HarmonyLib;
-//using Helpers;
-//using System;
-
-//namespace ButterEquipped.Patches;
-
-//[HarmonyPatch(typeof(InventoryScreenHelper), "CloseInventoryPresentation")]
-//internal static class InventoryScreenHelper_CloseInventoryPresentationPatch
-//{
-//    internal static event Action<bool>? OnClosing;
-
-//    public static void Prefix(/*object __instance, */bool fromCancel)
-//        => OnClosing?.Invoke(fromCancel);
-//}
-using HarmonyLib;
+﻿using HarmonyLib;
+using Helpers;
 using System;
-using TaleWorlds.CampaignSystem.Inventory;
 
 namespace ButterEquipped.Patches;
 
-//TaleWorlds.CampaignSystem.Inventory.InventoryManager.CloseInventoryPresentation(bool) : void @060012C4
-//// Token: 0x060012C4 RID: 4804 RVA: 0x000522C8 File Offset: 0x000504C8
-[HarmonyPatch(typeof(InventoryManager), nameof(InventoryManager.CloseInventoryPresentation))]
-internal static class InventoryManager_CloseInventoryPresentationPatch
+[HarmonyPatch(typeof(InventoryScreenHelper), "CloseInventoryPresentation")]
+internal static class InventoryScreenHelper_CloseInventoryPresentationPatch
 {
     internal static event Action<bool>? OnClosing;
 
-    public static void Prefix(object __instance, bool fromCancel)
+    public static void Prefix(/*object __instance, */bool fromCancel)
         => OnClosing?.Invoke(fromCancel);
 }
