1b72fd89caeab31b1ab6e58113bd380dbfe037c6
J. Zebedee <zebedee@code.gripe>
2025-09-18 21:43:07 -0500

    Make HeroEquipmentSet match EquipmentModes instead of IsInWarSet

--- src/ButterEquipped/AutoEquip/HeroEquipmentSet.cs
+++ src/ButterEquipped/AutoEquip/HeroEquipmentSet.cs
@@ -2,4 +2,6 @@
 
 namespace ButterEquipped.AutoEquip;
 
-public record HeroEquipmentSet([property: SaveableProperty(1)] string StringId, [property: SaveableProperty(2)] bool IsWarSet);
+public record HeroEquipmentSet(
+    [property: SaveableProperty(1)] string HeroStringId,
+    [property: SaveableProperty(2)] int SetMode); //TaleWorlds.CampaignSystem.ViewModelCollection.Inventory.SPInventoryVM.EquipmentModes
